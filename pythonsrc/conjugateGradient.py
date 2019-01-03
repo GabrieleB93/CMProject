Fixed a bug on the conjugate gradient. The direction had wrong sign.

pythonsrc/conjugateGradient.py
@@ -8,7 +8,6 @@ class conjugateGradient():
         self.feval = 1
         self.x = x if x != None else self.function.init_x()
         self.v, self.g = function.calculate(self.x)
-        self.g = -self.g
         self.ng = LA.norm(self.g)
         self.pOld = -1
         self.p = -self.g
@@ -34,7 +33,9 @@ class conjugateGradient():
                 break
 
             # calculate step along direction
-            alpha = self.function.conjugateGradientStepsize(self.p)
+            # -direction because model calculate the derivative of 
+            # phi' = f'(x-aplha*d)
+            alpha = self.function.conjugateGradientStepsize(-self.p)
             # step too short
             if alpha <= conf.mina:
                 self.status = 'error'
@@ -42,10 +43,9 @@ class conjugateGradient():
 
             self.oldgTg  = self.gTg
             ###########
-            self.x = self.x + alpha * self.g
+            self.x = self.x + alpha * self.p
             ###########
             self.v, self.g = self.function.calculate(self.x)
-            self.g = -self.g
             self.feval = self.feval + 1
             self.gTg = np.matmul(self.g.T, self.g)
             self.B = self.gTg/self.oldgTg

pythonsrc/normFunction.py
@@ -17,7 +17,7 @@ class normFunction():
         self.xQx = np.matmul(self.xT, self.Qx)
         f_x = self.xQx/self.xTx
         nabla_f = (2*x*f_x)/self.xTx - (2*self.Qx)/self.xTx 
-        self.f_x = f_x
+        self.f_x = f_x #it's -f(x)
         self.d = nabla_f
         self.dT = self.d.T
         return f_x, nabla_f